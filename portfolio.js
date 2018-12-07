f630b18261289ac57034ce265ae7fb3cdee2be1e
visakanj <visakanj@umich.edu>
2018-12-07 01:52:55 -0500

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,25 @@
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  
+    window.onscroll = function() {myFunction2()};
+
+    function myFunction2() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+    }
+    }
+}
+
